993934ba5953c2dd39b1d6cd03551e3123ed5bba
Joyce Lu <48322697+pumpjoy@users.noreply.github.com>
2023-04-02 17:17:41 +0800

    Add files via upload

--- pdf_jpg.py
+++ pdf_jpg.py
@@ -18,6 +18,8 @@ class MainWindow(QMainWindow):
         self.setFixedWidth(width)
         self.setFixedHeight(height)
 
+        self.list_of_file_paths = []
+
         # Button that prompts file selection 
         button = QPushButton("Select Files", self)
         button.move (700, 20)
@@ -28,17 +30,17 @@ class MainWindow(QMainWindow):
         self.label.setFont(QFont('Arial', 10))
         self.label.setGeometry(20,20,600,600)
         self.label.setAlignment(Qt.AlignTop)
-        self.label.setText("Some files")
+        self.label.setText("JPG files")
 
         # Second button that prompts to merge
-        button2 = QPushButton("jpg to pdf (merge)", self)
+        button2 = QPushButton("Convert", self)
         button2.move(700, 60)
         button2.clicked.connect(self.jpg_to_png)
 
         self.show()
     
     """
-    center and toggleFullScreen are for fullscreen 
+    center 
     """
     def center(self):
         qr = self.frameGeometry()
@@ -50,39 +52,44 @@ class MainWindow(QMainWindow):
     """
     for File open prompt with button press
     """
-    # TODO check if file is jpg, if not, do something
     def show_file_dialog(self):
         self.list_of_file_paths = []
         really_long_string = ''
         options = QFileDialog.Option()
         #options |= QFileDialog.DontUseNativeDialog
-        file_paths, _ = QFileDialog.getOpenFileNames(self, "Select Files", "", "All Files (*);;Image Files (*.png *.jpg *.bmp)", options=options)
+        file_paths, _ = QFileDialog.getOpenFileNames(self, "Select Files", "", "Image Files (*.jpg )", options=options)
         if file_paths:
             for file_path in file_paths:
-                self.list_of_file_paths.append(str(file_path))
-                really_long_string += file_path + '\n'
-            print(self.list_of_file_paths)
+                file_path = str(file_path)
+                if not file_path.endswith('.jpg'):
+                    self.label.setText("Only jpg files allowed.")
+                else:
+                    self.list_of_file_paths.append(file_path)
+                    really_long_string += file_path + '\n'
             self.label.setText(really_long_string)
 
     """
     Change jpg to png 
     """
     def jpg_to_png(self):
-        # creates a dialog to ask for new file name to be saved as pdf
-        options = QFileDialog.Options()
-        # options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getSaveFileName(self, "Save File", "", "All Files (*.pdf)", options=options)
-        
-        print (file_name)
-        # actual conversion
-        image_list = []
-        image1 = Image.open(self.list_of_file_paths[0])
-        image1.convert('RGB')
+        # check if theres convertible files in list
+        if len(self.list_of_file_paths) == 0:
+            self.label.setText("Need to have files to convert first")
+        else:
+            # creates a dialog to ask for new file name to be saved as pdf
+            options = QFileDialog.Options()
+            # options |= QFileDialog.DontUseNativeDialog
+            file_name, _ = QFileDialog.getSaveFileName(self, "Save File", "", "All Files (*.pdf)", options=options)
 
-        for file_path in self.list_of_file_paths[1:]:
-            image = Image.open(file_path)
-            image_list.append(image.convert('RGB'))
-        image1.save(file_name, save_all=True, append_images=image_list)
+            # actual conversion
+            image_list = []
+            image1 = Image.open(self.list_of_file_paths[0])
+            image1.convert('RGB')
+
+            for file_path in self.list_of_file_paths[1:]:
+                image = Image.open(file_path)
+                image_list.append(image.convert('RGB'))
+            image1.save(file_name, save_all=True, append_images=image_list)
         
         
 
